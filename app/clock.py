423e887c7f1e7a508e98a7454cf460c936f30f65
Federico Pacheco <fpacheco@fi.uba.ar>
2024-04-25 02:59:23 -0300

    print replaced by logging

--- app/clock.py
+++ app/clock.py
@@ -1,13 +1,28 @@
+import os
+import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
+logger = logging.getLogger("clock")
+# DEBUG, INFO, WARNING, ERROR, CRITICAL
+logging_level = os.environ.get("LOGGING_LEVEL", "DEBUG")
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging_level,
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print('This job is run every three minutes.')
+    logger.info('This job is run every three minutes.')
 
 
-print('Ready to start the scheduler...')
+@sched.scheduled_job('interval', minutes=1)
+def timed_job2():
+    logger.info('This job is run every one minute.')
 
+
+logger.info('Ready to start the scheduler...')
 sched.start()

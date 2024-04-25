73d2af56071c1df056e448270d4604e6744c31d2
Federico Pacheco <fpacheco@fi.uba.ar>
2024-04-25 01:48:44 -0300

    clock changed

--- app/clock.py
+++ app/clock.py
@@ -3,14 +3,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('This job is run every one minute.')
-
-
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
     print('This job is run every three minutes.')
 
+
 print('Ready to start the scheduler...')
+
 sched.start()

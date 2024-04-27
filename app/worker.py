90ff4e99fed32d19639e84194a184ce5169c5b10
Federico Pacheco <fpacheco@fi.uba.ar>
2024-04-27 00:27:47 -0300

    fix datetime log

--- app/worker.py
+++ app/worker.py
@@ -24,10 +24,7 @@ conn = redis.from_url(redis_url)
 if __name__ == '__main__':
     with Connection(conn):
         queue = Queue(connection=conn)
-        logger.info('Queue elements:', queue.count)
         queue.empty()
-        logger.info('Queue elements:', queue.count)
         worker = Worker(map(Queue, listen))
         worker.clean_registries()
-        logger.info('Ready to start the worker...')
         worker.work()

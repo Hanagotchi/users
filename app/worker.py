d370c25e29a8a7e95874dc57d1b89afa36de46c7
Federico Pacheco <fpacheco@fi.uba.ar>
2024-04-27 02:49:24 -0300

    Add UsersRepository to worker.py

--- app/worker.py
+++ app/worker.py
@@ -4,6 +4,7 @@ from arq.connections import RedisSettings
 from arq import Worker
 from httpx import AsyncClient
 from logs import init_logging
+from repository.Users import UsersRepository
 
 
 logger = init_logging('worker')
@@ -14,17 +15,22 @@ TIMEOUT_SECS_HTTX_CLIENT = 30
 async def startup(ctx: Worker) -> None:
     logger.info("Worker Started")
     ctx['session'] = AsyncClient(timeout=TIMEOUT_SECS_HTTX_CLIENT)
+    ctx['users_repository'] = UsersRepository()
 
 
 async def shutdown(ctx: Worker) -> None:
     logger.info("Worker end")
     await ctx['session'].aclose()
+    ctx['users_repository'].shutdown()
 
 
 async def heavy_endpoint(ctx: Worker, id_device: str):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logger.info(f"[{id_device} {now}] Calling heavy endpoint...")
     session: AsyncClient = ctx['session']
+    user_repository: UsersRepository = ctx['users_repository']
+    user_id_one = user_repository.get_user(1)
+    logger.info(f"[{id_device} {now}] User 1: {user_id_one}")
     secs_delay = 10
     url = 'https://httpbin.org/delay/%s' % secs_delay
     response = await session.get(url)

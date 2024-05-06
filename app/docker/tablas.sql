5a774961aadb1af5f2b95218df8fcee8b891aac1
Federico Jose Pacheco <51347305+fjpacheco@users.noreply.github.com>
2024-05-05 23:34:14 -0300

    [HAN-106] Configuración de scheduler en Heroku para notificaciones (alarmas) personalizables (#17)
    * fix async/sync functions
    
    * social_user is created in login/create_user && get endpoint changed: can now filter by lists of ids by query param.
    
    * fix get all users by ids funct
    
    * i miss cargo fmt.....
    
    * refactor map of ids
    
    * manual fix lint
    
    * manual fix lint  !
    
    * add random nickname generation
    
    Co-authored-by: violeta <50527601+violetaperezandrade@users.noreply.github.com>
    Co-authored-by: Sofia <sofeijoo@gmail.com>
    
    * add basic scheduled job
    
    * heroku yml changed
    
    * change to 3 minutes..
    
    * move the .py file....
    
    * move the .py file...
    
    * clock changed
    
    * add clock in entrypoint
    
    * print replaced by logging
    
    * enqueue dummy heavy function
    
    * add worker :)
    
    * add worker deployment
    
    * fix heroku.yml
    
    * fix error functions from main cannot be processed by worker...
    
    * fix import module 🙃
    
    * fix import module!
    
    * add args to enqueue
    
    * heavy_endpoint reduced
    
    * test wiyth async func :o
    
    * queue emptied
    
    * printf removed
    
    * fix count
    
    * fix worker queue
    
    * printf removed & add datetime.now
    
    * fix datetime log
    
    * burst mode
    
    * add asyncio wrapper
    
    * fix worker?
    
    * worker async!!! :D
    
    * 60 secs of interval scheduler
    
    * fix .yml
    
    * add dummy endpoint
    
    * prints removed
    
    * dummy endpoint changed
    
    * add timeout to httpx!
    
    * add pretty messages logs
    
    * fix env variables
    
    * Add UsersRepository to worker.py
    
    * test with entrypoint web
    
    * delete random file
    
    * fix lint
    
    * docs changed
    
    * add docs!
    
    * more docs
    
    * add alarm model & get_users_to_notify/delete_alarms in users repository!
    
    * add alarm_manager in worker!
    
    * add INTERVAL_SCHEDULE_SECS by environ
    
    * refactor scheduler: every 5 min triggers task !
    
    * add id_user not unique.
    
    * refactor log message
    
    * remove usless enviroment variable
    
    * fix lint
    
    * add utc datetime
    
    * add tz arg
    
    ---------
    
    Co-authored-by: violeta <50527601+violetaperezandrade@users.noreply.github.com>
    Co-authored-by: Sofia <sofeijoo@gmail.com>

--- app/docker/tablas.sql
+++ app/docker/tablas.sql
@@ -19,3 +19,33 @@ VALUES ('Agus', 'agus@fi.uba.ar', TO_DATE('1999-01-29', 'YYYY-MM-DD'), '{"lat": 
     ('Pach', 'pach@fi.uba.ar', TO_DATE('1999-08-06', 'YYYY-MM-DD'), '{"lat": 10, "long": 200}'),
     ('Sofi', 'sofi@fi.uba.ar', TO_DATE('1998-04-26', 'YYYY-MM-DD'), '{"lat": 1190, "long": 500}'),
     ('Violeta', 'violeta@fi.uba.ar', TO_DATE('1998-05-12', 'YYYY-MM-DD'), '{"lat": 330, "long": 2333}');
+
+
+-- Para users con device_token
+-- INSERT INTO
+--     users_service.users (name, email, birthdate, location, device_token)
+-- VALUES ('Agus', 'agus@fi.uba.ar', TO_DATE('1999-01-29', 'YYYY-MM-DD'), '{"lat": 20, "long": 100}', 'FEFEF'),
+--     ('Pach', 'pach@fi.uba.ar', TO_DATE('1999-08-06', 'YYYY-MM-DD'), '{"lat": 10, "long": 200}', 'ASDASD'),
+--     ('Sofi', 'sofi@fi.uba.ar', TO_DATE('1998-04-26', 'YYYY-MM-DD'), '{"lat": 1190, "long": 500}', 'QWEQWE'),
+--     ('Violeta', 'violeta@fi.uba.ar', TO_DATE('1998-05-12', 'YYYY-MM-DD'), '{"lat": 330, "long": 2333}', 'ZXZXZX');
+
+CREATE TABLE IF NOT EXISTS users_service.alarms (
+    id SERIAL PRIMARY KEY,
+    id_user INT NOT NULL REFERENCES users_service.users(id) ON DELETE CASCADE,
+    datetime TIMESTAMP WITH TIME ZONE NOT NULL,
+    content VARCHAR(128) NOT NULL
+);
+
+CREATE INDEX idx_alarms_datetime ON users_service.alarms(datetime);
+
+INSERT INTO
+    users_service.alarms (id_user, datetime, content)
+VALUES (1, '2024-04-29 02:15:00-03', 'Wake up 1!'),
+    (2, '2024-04-29 02:15:00-03', 'Wake up 2!'),
+    (3, '2024-04-29 02:25:00-03', 'Wake up 3!'),
+    (4, '2024-04-29 02:25:00-03', 'Wake up 4!');
+
+-- SELECT users_service.alarms.id, users_service.alarms.content, users_service.users.device_token
+-- FROM users_service.alarms 
+-- INNER JOIN users_service.users ON users_service.alarms.id_user = users_service.users.id 
+-- WHERE users_service.alarms.datetime = '2024-04-29 02:15:00-03';
--- entrypoint-heroku.sh
+++ entrypoint-heroku.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-uvicorn main:app --host 0.0.0.0 --port ${PORT}
+uvicorn main:app --host 0.0.0.0 --port ${PORT} &
+arq worker.WorkerSettings &
+python clock.py 

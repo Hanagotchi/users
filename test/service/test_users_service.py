b0f7d491ac832d8c21b583d1e00cb030c4775663
feijooso <sfeijoo@fi.uba.ar>
2024-05-12 14:38:47 -0300

    refactor

--- test/service/test_users_service.py
+++ test/service/test_users_service.py
@@ -1,19 +1,24 @@
+from datetime import date
+
 import pytest
 from unittest.mock import Mock, MagicMock
 
 from app.repository.Users import UsersRepository
+from app.schemas.Schemas import UserSchema
 from app.service.Users import UsersService
+
 
 @pytest.fixture
 def mock_user_repository():
     return Mock()
+
 
 @pytest.fixture
 def users_service(mock_user_repository):
     return UsersService(mock_user_repository)
 
 
-def getMock(classToMock, attributes=None):
+def get_mock(classToMock, attributes=None):
     if attributes is None:
         attributes = {}
     mock = MagicMock(spec=classToMock)
@@ -21,12 +26,26 @@ def getMock(classToMock, attributes=None):
     return mock
 
 
+john = UserSchema(
+    id=1,
+    name="John",
+    email="john@mail.com",
+    gender="male",
+    photo="link.com",
+    birthdate=date(1990, 5, 12),
+    location={},
+    nickname="john",
+    biography="un tipo",
+    device_token="d3vic2to0k3n"
+)
+
+
 def test_get_user(mock_user_repository):
     attr_db = {
-        "get_user.return_value":  {"id": 1, "name": "John"}}
-    mock_db = getMock(UsersRepository, attr_db)
+        "get_user.return_value": john}
+    mock_db = get_mock(UsersRepository, attr_db)
     service = UsersService(mock_db)
     user = service.get_user(1)
 
-    assert user == {"id": 1, "name": "John"}
+    assert user == john
     mock_db.get_user.assert_called_once_with(1)
